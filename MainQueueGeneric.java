20d88023cefbe5f0f1fce1665f519f5bd6c82706
siska1 <44000389+siska1@users.noreply.github.com>
2018-11-01 17:24:17 +0700

    Create MainQueueGeneric.java

---
+++ MainQueueGeneric.java
@@ -0,0 +1,15 @@
+
+public class MainQueueGeneric {
+
+			public static void main(String[] args) {
+				QueueGeneric <Integer> sc = new QueueGeneric <Integer>();
+
+							    sc.insert(1);
+							    sc.insert(2);
+							    sc.insert(3);
+							    System.out.print("Hasil :");
+							    System.out.println(sc.remove());
+
+			}
+
+		}

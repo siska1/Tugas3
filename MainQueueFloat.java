e99e1abc03d34df684f9deeabb97f0dd0983d7d5
siska1 <44000389+siska1@users.noreply.github.com>
2018-11-01 17:22:37 +0700

    Create MainQueueFloat.java

---
+++ MainQueueFloat.java
@@ -0,0 +1,15 @@
+
+public class MainQueueFloat {
+	
+			public static void main(String[] args) {
+				QueueFloat antrian = new QueueFloat();
+				antrian.cetak();
+				antrian.insert(145); antrian.cetak();
+				antrian.insert(150); antrian.cetak();
+				antrian.insert(156); antrian.cetak();
+				antrian.insert(166); antrian.cetak();
+
+			}
+
+
+	}
